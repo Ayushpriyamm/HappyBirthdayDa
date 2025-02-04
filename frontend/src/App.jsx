f5e627cef774186c5e7ec7a5fded2f48fcf3d1a2
Ayush Priyam <ayushpriyam24@gmail.com>
2025-02-04 22:51:22 +0530

    intilize project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-center text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
